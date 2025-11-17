a0ed3b5739d24b08534e90c54a851c633ec8c36e
Antonina Frąckowiak <tosiamf@gmail.com>
2025-11-17 15:08:35 +0100

    Added approximation template header

---
+++ headers/approximation.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "graph.h"
+#include "multiEdge.h"
+#include <vector>
+#include <tuple>
+
+std::tuple<int, std::vector<std::vector<int>>> findCopiesApproximation(Graph &G, Graph &H, int numCopies);
+
+std::vector<int> findDenseSubgraph(Graph &G);
+std::tuple<std::vector<int>, std::vector<MultiEdge>> findMatch(Graph &G, Graph &H, const std::vector<int> &denseSubgraph);
+void pickAndRemoveVertex(Graph &G, const std::vector<int> &match);
