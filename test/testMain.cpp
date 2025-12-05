8057e99c7896ce9b922f731ae98a46d24e352af5
annap <anna.piasecka09@gmail.com>
2025-12-05 16:56:44 +0100

    Refactor tests

--- test/testMain.cpp
+++ test/testMain.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
-#include <filesystem>
 #include <fstream>
 #include <string>
 #include <set>
-
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "./test_utils/testRunner.h"
 
 int main()
 {
-    fs::path inputFolderExactAlgorithm = "test/exact_algorithm/input_data";
-    fs::path expectedFolderExactAlgorithm = "test/exact_algorithm/expected_results";
-    fs::path outputFolderExactAlgorithm = "test/exact_algorithm/output_results";
+    std::string inputFolderExactAlgorithm = "test/exact_algorithm/input_data";
+    std::string expectedFolderExactAlgorithm = "test/exact_algorithm/expected_results";
+    std::string outputFolderExactAlgorithm = "test/exact_algorithm/output_results";
 
-    fs::path inputFolderApproximationAlgorithm = "test/approximation_algorithm/input_data";
-    fs::path expectedFolderApproximationAlgorithm = "test/approximation_algorithm/expected_results";
-    fs::path outputFolderApproximationAlgorithm = "test/approximation_algorithm/output_results";
-
-    fs::create_directories(outputFolderExactAlgorithm);
-    fs::create_directories(outputFolderApproximationAlgorithm);
+    std::string inputFolderApproximationAlgorithm = "test/approximation_algorithm/input_data";
+    std::string expectedFolderApproximationAlgorithm = "test/approximation_algorithm/expected_results";
+    std::string outputFolderApproximationAlgorithm = "test/approximation_algorithm/output_results";
 
     std::cout << TestRunner::COLOR_BLUE_BOLD << "\nStarted tests for Exact Algorithm:\n"
               << TestRunner::COLOR_RESET;
-    auto [passedExact, totalExact] = TestRunner::runAllTests(inputFolderExactAlgorithm, expectedFolderExactAlgorithm, outputFolderExactAlgorithm, false);
+    auto [passedExact, totalExact] = TestRunner::runAllTests(
+        inputFolderExactAlgorithm,
+        expectedFolderExactAlgorithm,
+        outputFolderExactAlgorithm,
+        false);
 
     std::cout << TestRunner::COLOR_BLUE_BOLD << "\nStarted tests for Approximation Algorithm:\n"
               << TestRunner::COLOR_RESET;
-    auto [passedApproximation, totalApproximation] = TestRunner::runAllTests(inputFolderApproximationAlgorithm, expectedFolderApproximationAlgorithm, outputFolderApproximationAlgorithm, true);
+    auto [passedApproximation, totalApproximation] = TestRunner::runAllTests(
+        inputFolderApproximationAlgorithm,
+        expectedFolderApproximationAlgorithm,
+        outputFolderApproximationAlgorithm,
+        true);
 
     std::cout << "\nResults for Exact Algorithm:\n";
     TestRunner::showTotalResults(passedExact, totalExact);
